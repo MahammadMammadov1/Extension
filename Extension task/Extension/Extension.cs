ddfa99efb10e8ee4b9ea109d401100fdcae5c441
MahammadMammadov1 <bd6mhrhq0@code.edu.az>
2023-10-18 10:16:34 -0700

    all task done

---
+++ Extension task/Extension/Extension.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_task.Extension
+{
+    public static class Extension
+    {
+        public static void WordCount(this string sentence , string word) 
+        {
+            //TASK 1
+
+            string[] newArr = sentence.Split(' ');
+            int count = 0;
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                if (newArr[i] == word)
+                {
+                    count ++;
+                }
+            }
+            Console.WriteLine($"Daxil olunan soz bu cumlede {count} defe tekrarlanmisdir !!! ");
+        }
+
+
+        //TASK 2
+
+        public static bool IsPalindrome(this int num)
+        {
+            int num1 = num;
+            int reverseNum = 0;
+
+            while (num > 0)
+            {
+                int digit = num % 10;
+                reverseNum = reverseNum * 10 + digit;
+                num /= 10;
+            }
+
+            if(num1 ==reverseNum) return true;
+            else return false;
+        }
+
+
+        //TASK 3
+
+
+        public static bool HasAnyDigit(this string word)
+        {
+            for (int i = 0;i < word.Length;i++)
+            {
+                if (char.IsDigit(word[i])) return true;    
+            }
+            return false;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Extension task/Program.cs
@@ -0,0 +1,29 @@
+﻿using Extension_task.Extension;
+
+namespace Extension_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //TASK 1
+
+            string sentence = "Bugun hava cox cox yaxsidir ";
+            sentence.WordCount("cox");
+
+            //TASK 2
+
+            int num = 3553;
+            Console.WriteLine(num.IsPalindrome());
+
+
+            //TASK 3
+
+            string word = "Salam123";
+            Console.WriteLine(word.HasAnyDigit());
+
+
+        }
+    }
+}
